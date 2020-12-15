56227a5786df7676630a06d3ab46110e8f3abe04
Yohan Ishara <yohanishara01@gmail.com>
2020-12-15 12:52:16 +0530

    Customer Servlet Validation compleated

--- src/lk/ijse/dep/web/api/CustomerServlet.java
+++ src/lk/ijse/dep/web/api/CustomerServlet.java
@@ -2,6 +2,8 @@ package lk.ijse.dep.web.api;
 
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbException;
+import jakarta.json.stream.JsonParsingException;
 import lk.ijse.dep.web.model.Customer;
 import org.apache.commons.dbcp2.BasicDataSource;
 
@@ -30,15 +32,15 @@ public class CustomerServlet extends HttpServlet {
         BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
         //response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setContentType("application/json");
-        try (PrintWriter out = response.getWriter()) {
-            try {
+        try (PrintWriter out = response.getWriter();Connection con = cp.getConnection()) {
+
                /* Class.forName("com.mysql.jdbc.Driver");
                 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "1234");*/
-                String id= req.getParameter("id");
-                Connection con = cp.getConnection();
-                PreparedStatement pstm = con.prepareStatement("SELECT * FROM Customer"+((id!=null)? " WHERE id=?":""));
-                if (id!=null){
-                    pstm.setObject(1,id);
+                String id = req.getParameter("id");
+
+                PreparedStatement pstm = con.prepareStatement("SELECT * FROM Customer" + ((id != null) ? " WHERE id=?" : ""));
+                if (id != null) {
+                    pstm.setObject(1, id);
                 }
                 ResultSet rst = pstm.executeQuery();
                 List<Customer> customerList = new ArrayList<>();
@@ -48,83 +50,142 @@ public class CustomerServlet extends HttpServlet {
                     String address = rst.getString(3);
                     customerList.add(new Customer(id, name, address));
                 }
-                Jsonb jsonb = JsonbBuilder.create();
-                out.println(jsonb.toJson(customerList));
-                con.close();
+
+                if (id != null && customerList.isEmpty()) {
+                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                } else {
+                    Jsonb jsonb = JsonbBuilder.create();
+                    out.println(jsonb.toJson(customerList));
+                    con.close();
+                }
             } catch (SQLException exception) {
                 exception.printStackTrace();
-            }
-
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Jsonb Jsonb=JsonbBuilder.create();
-        Customer customer = Jsonb.fromJson(request.getReader(), Customer.class);
+
         //response.addHeader("Access-Control-Allow-Origin","http://localhost:3306");
-        response.setContentType("application/json");
         BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
-        try {
-            Connection connection = cp.getConnection();
+        try ( Connection connection = cp.getConnection();) {
+            Jsonb Jsonb = JsonbBuilder.create();
+            Customer customer = Jsonb.fromJson(request.getReader(), Customer.class);
+
+            if (customer.getId() == null || customer.getName() == null || customer.getAddress() == null) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
+
+            if (!customer.getId().matches("C\\d{3}") || customer.getName().isEmpty() || customer.getAddress().isEmpty()) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
+
             PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?)");
             pstm.setString(1, customer.getId());
             pstm.setString(2, customer.getName());
             pstm.setString(3, customer.getAddress());
-            boolean success=pstm.executeUpdate()>0;
+            boolean success = pstm.executeUpdate() > 0;
 
-            if(success){
-                response.getWriter().println(Jsonb.toJson(true));
-            }else {
-                response.getWriter().println(Jsonb.toJson(false));
+            if (success) {
+                response.setStatus(HttpServletResponse.SC_CREATED);
+            } else {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             }
             // Jsonb.toJson(true);
             connection.close();
+        }catch (SQLIntegrityConstraintViolationException ex){
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-            response.getWriter().println(Jsonb.toJson(false));
+        }catch (JsonParsingException e){
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       String id=req.getParameter("id");
+      //CORS Policy/
+        resp.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+        String id = req.getParameter("id");
+        if (id == null || !id.matches("C\\d{3}")){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
         BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
-        try {
-            Connection connection = cp.getConnection();
-            PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
+        try (Connection connection = cp.getConnection()){
+            PreparedStatement pstm = connection.prepareStatement("SELECT  * FROM Customer WHERE id=?");
             pstm.setObject(1, id);
-            boolean success=pstm.executeUpdate()>0;
-            if (success){
-                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            if(pstm.executeQuery().next()) {
+                pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
+                pstm.setObject(1, id);
+                boolean success = pstm.executeUpdate() > 0;
+                if (success) {
+                    resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                }
             }else {
-                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
             }
-            connection.close();
+        } catch (SQLIntegrityConstraintViolationException ex){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
         } catch (SQLException throwables) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             throwables.printStackTrace();
         }
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       String id = req.getParameter("id");
-         Jsonb jsonb = JsonbBuilder.create();
-        Customer customer = jsonb.fromJson(req.getReader(), Customer.class);
+        //CORS Policy/
+        resp.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+        String id = req.getParameter("id");
+        if (id == null || !id.matches("C\\d{3}")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
         BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
-        try {
-            Connection connection= cp.getConnection();
-            PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?,address=? WHERE id=?");
-            pstm.setObject(1, customer.getName());
-            pstm.setObject(2, customer.getAddress());
-            pstm.setObject(3, id);
-            if (pstm.executeUpdate()>0){
-                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
-            }else{
-                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        try (Connection connection = cp.getConnection()) {
+            Jsonb jsonb = JsonbBuilder.create();  //entry point
+            Customer customer = jsonb.fromJson(req.getReader(), Customer.class);
+
+            ///validate/
+            if (customer.getId() != null || customer.getName() == null || customer.getAddress() == null) {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
+
+            if (customer.getName().trim().isEmpty() || customer.getAddress().trim().isEmpty()) {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Customer WHERE id=?");
+            pstm.setObject(1, id);
+            if (pstm.executeQuery().next()) {
+                pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
+                pstm.setObject(1, customer.getName());
+                pstm.setObject(2, customer.getAddress());
+                pstm.setObject(3, id);
+                if (pstm.executeUpdate() > 0) {
+                    resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                }
+            } else {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        } catch (JsonbException exp) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
     }
 }
